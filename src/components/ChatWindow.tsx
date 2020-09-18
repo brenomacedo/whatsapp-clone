963bcdd72daf782cb305ad80f551385a8634c442
brenosekiro <b88660501@gmail.com>
2020-09-18 18:23:04 -0300

    messageAndMicrophoneRecognition

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { FormEvent, useState, ChangeEvent } from 'react'
 import Search from '@material-ui/icons/Search'
 import Attach from '@material-ui/icons/AttachFile'
 import More from '@material-ui/icons/More'
+import InsertEmoticon from '@material-ui/icons/InsertEmoticon'
+import Send from '@material-ui/icons/Send'
+import Close from '@material-ui/icons/Close'
+import Mic from '@material-ui/icons/Mic'
+import EmojiPicker, { IEmojiData } from 'emoji-picker-react'
 import './ChatWindow.css'
 
 const ChatWindow = () => {
+
+    let recognition: SpeechRecognition | null = null;
+    let SpeechRecognition = window.SpeechRecognition
+    if(SpeechRecognition !== undefined) {
+        recognition = new SpeechRecognition()
+    }
+
+    const handleEmojiClick = (e: MouseEvent, emoji: IEmojiData) => {
+        setText(text + emoji.emoji)
+    }
+
+    const [emojiOpen, setEmojiOpen] = useState(false)
+    const [text, setText] = useState('')
+    const [listening, setListening] = useState(false)
+
+    const handleMicClick = () => {
+        if(recognition !== null) {
+            recognition.onstart = () => {
+                setListening(true)
+            }
+
+            recognition.onend = () => {
+                setListening(false)
+            }
+            recognition.onresult = e => {
+                setText(e.results[0][0].transcript)
+            }
+            recognition.start()
+        }
+    }
+
+    const handleSendClick = () => {
+
+    }
+
     return (
         <div className="chatWindow">
             <div className="chatWindow-header">
@@ -32,8 +72,46 @@ const ChatWindow = () => {
             <div className="chatWindow-body">
 
             </div>
-            <div className="chatWindow-footer">
 
+            <div className="chatWindow-emojiarea" style={{ height: emojiOpen ? 200 : 0 }}>
+                <EmojiPicker disableSearchBar onEmojiClick={handleEmojiClick} />
+            </div>
+
+            <div className="chatWindow-footer">
+                <div className="chatWindow-pre">
+
+                    <div onClick={() => setEmojiOpen(true)} className="chatWindow-btn">
+                        <InsertEmoticon style={{ color: emojiOpen ? '#009688' : '#919191' }} />
+                    </div>
+
+                    <div style={{
+                        width: emojiOpen ? 40 : 0
+                    }} onClick={() => setEmojiOpen(false)} className="chatWindow-btn">
+                        <Close style={{ color: '#919191' }} />
+                    </div>
+
+                </div>
+
+                <div className="chatWindow-inputarea">
+                    <input value={text} onChange={e => setText(e.target.value)}
+                    placeholder="Digite uma mensagem" type="text" className="chatWindow-input"/>
+                </div>
+
+                <div className="chatWindow-pos">
+
+                    {text === '' ? (
+                        <div onClick={handleMicClick} className="chatWindow-btn">
+                            <Mic style={{ color: listening ? '#126ece' : '#919191' }} />
+                        </div>
+                    ) : (
+                        <div onClick={handleSendClick} className="chatWindow-btn">
+                            <Send style={{ color: '#919191' }} />
+                        </div>
+                    )}
+
+
+
+                </div>
             </div>
         </div>
     )
